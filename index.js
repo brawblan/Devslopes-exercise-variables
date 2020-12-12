f781e04f1e0e4f895fd6a2350bc732def084ca33
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2020-12-12 13:44:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var car = {
+  paintColor: 'black', //string
+  make: 'Honda', //string
+  model: 'Accord', //string
+  year: 2010, //number
+  isManualTransmission: false, //boolean
+  seats: 5, //number
+  milage: 260000, // number
+}
+
+console.log('Value: ' , car.paintColor, typeof car.paintColor);
+console.log('Value: ' , car.make, typeof car.make);
+console.log('Value: ' , car.model, typeof car.model);
+console.log('Value: ' , car.year, typeof car.year);
+console.log('Value: ' , car.isManualTransmission, typeof car.isManualTransmission);
+console.log('Value: ' , car.seats, typeof car.seats);
+console.log('Value: ' , car.milage, typeof car.milage);
+console.log(car);
